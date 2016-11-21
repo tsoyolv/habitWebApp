e9689ac2bbda8b9e78f92886fdcf2db8a37891a1
Oleg <tsoyolv@gmail.com>
2016-11-21 23:28:31 +0300

    fix. fixed resources folder
    features
    1. added 1.8 java support
    2. added custom internationalization via txt file and concurrent cache

--- src/main/java/com/habit/custom/server/i18n/Internationalization.java
+++ src/main/java/com/habit/custom/server/i18n/Internationalization.java
@@ -1,10 +1,14 @@
 package com.habit.custom.server.i18n;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 /**
- * Created by Oleg on 20.11.2016.
+ * OLTS on 20.11.2016.
  */
 // Singleton
 public class Internationalization {
@@ -21,7 +25,28 @@ public class Internationalization {
         if (cache.size() > CACHE_SIZE) {
             cache.clear();
         }
-        return ";";
+        String resourceFromFile = getResourceFromFile(key);
+        cache.put(key, resourceFromFile);
+        return resourceFromFile;
+    }
+
+    private static String getResourceFromFile(String key) {
+        final String PATH = "custom/l10n/english.txt";
+        final String SPLITTER = ":";
+        InputStream resourceAsStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(PATH);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(resourceAsStream))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] split = line.split(SPLITTER);
+                if (split[0].equals(key)) {
+                    return split[1];
+                }
+            }
+
+        } catch (IOException x) {
+            return "FAKE";
+        }
+        return "";
     }
 
     private static Internationalization instance;
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,7 +2,8 @@
 pageEncoding="ISO-8859-5"%>
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2><%= Internationalization.getInstance().getResource("1") %></h2>
 <a href="hello">click</a>
 </body>
+<%@ page import="com.habit.custom.server.i18n.Internationalization" %>
 </html>
