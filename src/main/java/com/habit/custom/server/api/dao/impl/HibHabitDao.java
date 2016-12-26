446eb00fcfb65c07d937cb42a15af96540d66797
Oleg <tsoyolv@gmail.com>
2016-12-26 19:24:32 +0400

    hibernate config ended + habitDao via hibernate

--- src/main/java/com/habit/custom/server/api/dao/impl/HibHabitDao.java
+++ src/main/java/com/habit/custom/server/api/dao/impl/HibHabitDao.java
@@ -5,18 +5,42 @@ import com.habit.custom.server.api.model.Habit;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * OLTS on 25.12.2016.
  */
 @Repository("hibHabDao")
 @EnableTransactionManagement
+@Transactional
 public class HibHabitDao implements HabitDao {
 
     @Autowired
+    @Qualifier("sessionFactory")
     private SessionFactory sessionFactory;
+
+    @Override
+    // @Transactional   also for methods
+    public Habit get(int habitId) {
+        return getCurrentSession().get(Habit.class, habitId);
+    }
+
+    @Override
+    public Habit create(Habit templateHabit) {
+        return (Habit) getCurrentSession().save(templateHabit);
+    }
+
+    @Override
+    public void delete(int habitId) {
+        getCurrentSession().delete(habitId);
+    }
+
+    private Session getCurrentSession() {
+        return sessionFactory.getCurrentSession();
+    }
 
     public SessionFactory getSessionFactory() {
         return sessionFactory;
@@ -24,24 +48,5 @@ public class HibHabitDao implements HabitDao {
 
     public void setSessionFactory(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
-    }
-
-    @Override
-    public Habit get(int habitId) {
-        return getCurrentSession().get(Habit.class, habitId);
-    }
-
-    @Override
-    public Habit create(Habit templateHabit) {
-        return null;
-    }
-
-    @Override
-    public void delete(int habitId) {
-
-    }
-
-    private Session getCurrentSession() {
-        return sessionFactory.getCurrentSession();
     }
 }
