1fe9bda7aa0cea19afac8817f7d505834cc1ae3d
Oleg <tsoyolv@gmail.com>
2017-02-14 22:51:17 +0400

    Adding Functionality to Existing Thread-safe Classes. Client-side locking. The problem is that it synchronizes on the wrong lock.

---
+++ src/main/java/com/habit/custom/knowledges/thread/ListHelper.java
@@ -0,0 +1,22 @@
+package com.habit.custom.knowledges.thread;
+
+import javax.annotation.concurrent.NotThreadSafe;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * The problem is that it synchronizes on the
+ wrong lock.
+ *  */
+@NotThreadSafe
+public class ListHelper<E> {
+    public List<E> list = Collections.synchronizedList(new ArrayList<E>());
+
+    public synchronized boolean putIfAbsent(E x) {
+        boolean absent = !list.contains(x);
+        if (absent)
+            list.add(x);
+        return absent;
+    }
+}
